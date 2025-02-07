5f82a880005bf28ee6ea24567d87ee298f7c3924
Danil Zholobov <asusmemp10@gmail.com>
2025-02-07 20:57:36 +0300

    Начал новую тему про потоковый ввод и вывод и сделал пару хреней

---
+++ Выходной файл/Выходной файл.cpp
@@ -0,0 +1,14 @@
+﻿#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m1 = 1, m2 = 23;
+    char h = 'x';
+    string str1 = "Вопросики возникают";
+    ofstream outfile("VIHOD.txt");
+    outfile << h << m1 << '\n' << m2 << ' ' << str1;
+    cout << "Файл записан";
+}
